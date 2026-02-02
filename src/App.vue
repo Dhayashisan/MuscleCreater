ea17a6abe21097df053dc17ed3eeccb74f255c5f
Dhayashisan <124221210+Dhayashisan@users.noreply.github.com>
2026-02-02 05:26:02 +0000

    これから始まる

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <script setup></script>
+<!--マッスル個人的プログラム-->
+<!--簡易設計-->
+<!--画面は「当日記入画面」「編集画面」「メンテナンス画面」「グラフ画面」-->
+<!--★ボディビルを重視したトレーニングを行う為、以下流れを行えるよう意識づける為の管理表。-->
+<!--０：アップ　１：意識づけ（フォーム）　２：個人で気を付けること　３：前回の重さ・回数　４：感想-->
+<!--ストップウォッチ機能がいるかどうか迷う-->
+<!--トレーニング登録タイプ：7ぐらい題名は任せる。-->
+
+<!--★気を付けること-->
+<!--淡々とトレーニングをこなせる様、余計な機能を省くこと-->
 
 <template>
-  <h1>Welcome!!!PokochinMuscleSchoooooooooooooool!!!!</h1>
-  <p>
-  benchpress
-  wordpress
-  squat
-  deadlift
-  bicepcurl
-  tricep
-  </p>
+  <main>
+    <div class="header">
+      <h1>Welcome!!!PokochinMuscleSchoooooooooooooool!!!!</h1>
+    </div>
+    <div class="top"><h1>Menu</h1></div>
+    <div class="main-contents">
+      <button>Traning</button>
+      <button>Edit</button>
+      <button>Maintenance</button>
+      <button>Graph</button>
+    </div>
+    <div class="sub-contents"></div>
+    <div class="bottom"></div>
+    <div class="footer"></div>
+  </main>
 </template>
 
 <style scoped></style>
